5999c42bbbaf833a2c5e7144396770b968e6a928
Newton <newtonkagimbi11@gmail.com>
2024-01-02 22:51:49 +0300

    Added JwtService to help in extracting username from jwt

--- src/main/java/com/emtech/employeemanagement/auth/jwt/JwtService.java
+++ src/main/java/com/emtech/employeemanagement/auth/jwt/JwtService.java
@@ -1,12 +1,38 @@
 package com.emtech.employeemanagement.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     public String extractUsername(String jwt) {
-        return null;
+        return extractClaim(jwt, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwt){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
