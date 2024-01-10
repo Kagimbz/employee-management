bd4a67be45037d7b4b2d148f880eacc988e190b4
Newton <newtonkagimbi11@gmail.com>
2024-01-10 22:13:51 +0300

    Added methods for registering and authenticating users

---
+++ src/main/java/com/emtech/employeemanagement/auth/authentication/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.emtech.employeemanagement.auth.authentication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String jwt;
+}
