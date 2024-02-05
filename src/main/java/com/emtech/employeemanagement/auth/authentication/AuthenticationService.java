5b255e3247b0340e8807905d16a6ecaeb9e277c4
Newton <newtonkagimbi11@gmail.com>
2024-02-05 09:30:40 +0300

    Make minor change to password saved during user registration

--- src/main/java/com/emtech/employeemanagement/auth/authentication/AuthenticationService.java
+++ src/main/java/com/emtech/employeemanagement/auth/authentication/AuthenticationService.java
@@ -27,7 +27,7 @@ public class AuthenticationService {
                         .firstName(request.getFirstName())
                         .lastName(request.getLastName())
                         .email(request.getEmail())
-                        .password(passwordEncoder.encode(request.getEmail()))
+                        .password(passwordEncoder.encode(request.getPassword()))
                         .role(Role.ADMIN)
                 .build();
         repository.save(user);
