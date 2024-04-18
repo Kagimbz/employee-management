fd4de8644233aed0627f039c5c64fbf2659e4c15
Newton <newtonkagimbi11@gmail.com>
2024-04-18 14:15:27 +0300

    Replace explicit getter with lombok annotation

--- src/main/java/com/emtech/employeemanagement/CustomAppender.java
+++ src/main/java/com/emtech/employeemanagement/CustomAppender.java
@@ -2,10 +2,12 @@ package com.emtech.employeemanagement;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class CustomAppender extends AppenderBase<ILoggingEvent> {
     List<ILoggingEvent> logs = new ArrayList<>();
 
@@ -14,7 +16,4 @@ public class CustomAppender extends AppenderBase<ILoggingEvent> {
         logs.add(event);
     }
 
-    public List<ILoggingEvent> getLogs() {
-        return logs;
-    }
 }
